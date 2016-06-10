2b792b4a21a702b5a297548a22a79d24e134000f
Kuroiwa Nobuhiro <Kuroiwa Nobuhiro>
2016-06-11 00:36:25 +0900

    forgot to add header

---
+++ src/OsakanaFft/include/OsakanaFpFftDebug.h
@@ -0,0 +1,25 @@
+#ifndef _DEBUG_H_
+#define _DEBUG_H_
+
+#define DEBUG_OUTPUT_NUM    512
+#if defined(_DEBUG)
+#define DLOG(fmt, ...) fprintf(stderr, fmt, __VA_ARGS__);
+#define DCOMPLEX(cs, num) 	for (int i = 0; i < num; i++) { \
+								fp_complex_str(&cs[i], buf, sizeof(buf)); \
+								DLOG("%s\n", buf); \
+							}
+#define DFPS(fps, num)		for (int i = 0; i < num; i++) { \
+								Fp2CStr(fps[i], buf, sizeof(buf)); \
+								printf("%s\n", buf); \
+							}
+#define DRAWDATA(x, num)	for (int i = 0; i < num; i++) { \
+								DLOG("%u\n", x[i].re); \
+							}
+#else
+#define DLOG(fmt, ...)
+#define DCOMPLEX(cs, num)
+#define DFPS(fps, num)
+#define DRAWDATA(x, num)
+#endif
+
+#endif
