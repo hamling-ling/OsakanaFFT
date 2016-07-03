989269308f258318b389971e4357fc46afdfe47c
Kuroiwa Nobuhiro <Kuroiwa Nobuhiro>
2016-07-03 15:26:42 +0900

    bat file to make codes for gadget renesas project

--- tool/release/release_renesas.bat
+++ tool/release/release_renesas.bat
@@ -1,8 +1,12 @@
 set BATDIR=%~DP0
 set DSTROOT=%BATDIR%\..\..\release_src
 set OSAKANAFFT_DSTDIR=%DSTROOT%\OsakanaFFT
+set PITCHDETECTOR_DSTDIR=%DSTROOT%\PitchDetector
 
 rd /S /Q %DSTROOT%
 mkdir %OSAKANAFFT_DSTDIR%\include
 robocopy %BATDIR%\..\..\src\OsakanaFFT\include %OSAKANAFFT_DSTDIR%\include
 robocopy %BATDIR%\..\..\src\OsakanaFFT\src %OSAKANAFFT_DSTDIR%\src
+
+robocopy %BATDIR%\..\..\src\OsakanaPitchDetection\src %PITCHDETECTOR_DSTDIR%\src
+robocopy %BATDIR%\..\..\src\OsakanaPitchDetection\include %PITCHDETECTOR_DSTDIR%\include
