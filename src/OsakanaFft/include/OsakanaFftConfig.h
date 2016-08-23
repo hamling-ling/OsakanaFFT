d7445a577945c3e6225f762a2e8fc3096a35b103
Kuroiwa Nobuhiro <tacobell@gj9.so-net.ne.jp>
2016-08-24 02:01:00 +0900

    add N=256 Q15.16 directive

--- src/OsakanaFft/include/OsakanaFftConfig.h
+++ src/OsakanaFft/include/OsakanaFftConfig.h
@@ -7,12 +7,17 @@
 #define USE_TWIDDLE_TABLE_N512
 #endif
 
-#if 1 // N=512, fixed Q1.14 fixed point
+#if 1 // N=256, fixed Q1.14 fixed point
 #define _USE_Q1_14_FIXEDPOINT
 #define USE_BIT_REVERSE_N256
 #define USE_TWIDDLE_TABLE_N256
-
 #endif
+
+#if 0 // N=256, fixed Q15.16 fixed point
+#define USE_BIT_REVERSE_N256
+#define USE_TWIDDLE_TABLE_N256
+#endif
+
 #if 0 // N=1024, floating point
 
 #endif
