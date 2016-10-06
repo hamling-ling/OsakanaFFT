d280bf644c2b62d7f5756c66a9285eb50f09ebe2
Kuroiwa Nobuhiro <tacobell@gj9.so-net.ne.jp>
2016-10-07 00:46:23 +0900

    add pitch detection unittest

--- src/OsakanaPitchDetectionTest/unittest1.cpp
+++ src/OsakanaPitchDetectionTest/unittest1.cpp
@@ -1,22 +1,62 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
-#include "../OsakanaPitchDetection/src/PeakDetectMachineFp.h"
+#include "OsakanaPitchDetection.h"
+#include "OsakanaPitchDetectionFp.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
+static int readFpData(Fp_t* data, uint8_t stride, const int dataNum, Fp_t* rawdata_min, Fp_t* rawdata_max)
+{
+
+	return 0;
+}
+
+static int readData(float* data, uint8_t stride, const int dataNum)
+{
+
+	return 0;
+}
+
 namespace OsakanaPitchDetectionTest
-{		
+{	
 	TEST_CLASS(UnitTest1)
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(TestDestructWithoutInit)
 		{
-			MachineContextFp_t* mctx = NULL;
-			mctx = CreatePeakDetectMachineContextFp();
-			Assert::IsNotNull(mctx);
-			DestroyPeakDetectMachineContextFp(mctx);
+			{
+				PitchDetectorFp detector;
+			}
+			{
+				PitchDetector detector;
+			}
 		}
 
+		TEST_METHOD(TestCleanupWithoutInit)
+		{
+			{
+				PitchDetectorFp detector;
+				detector.Cleanup();
+			}
+			{
+				PitchDetector detector;
+				detector.Cleanup();
+			}
+		}
+
+		TEST_METHOD(TestFpFft)
+		{
+			PitchDetectorFp detector;
+			detector.Initialize(readFpData);
+			detector.DetectPitch();
+		}
+
+		TEST_METHOD(TestFft)
+		{
+			PitchDetector detector;
+			detector.Initialize(readData);
+			detector.DetectPitch();
+		}
 	};
 }
